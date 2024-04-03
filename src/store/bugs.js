3bd0fa94c0ce9c77965b7fe6a55605477ccbaaee
Shanmukh459 <saikrrish459@gmail.com>
2024-04-03 12:26:25 -0700

    refactored the actions and the reducer using createSlice

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,43 +1,25 @@
-import { createAction, createReducer } from "@reduxjs/toolkit"
+import { createAction, createReducer, createSlice } from "@reduxjs/toolkit"
 
-export const bugAdded = createAction("bugAdded")
-export const bugRemoved = createAction("bugRemoved")
-export const bugResolved = createAction("bugResolved")
-
-//Reducer
 let lastIndex = 0
 
-export default createReducer([], {
-    //key: value
-    //action: function (event => event handlers)
-    [bugAdded.type]: (bugs, action) => {
-        bugs.push({
-            id: ++lastIndex,
-            description: action.payload.description,
-            resolved: false
-        })
-    },
-
-    [bugResolved.type]: (bugs, action) => {
-        const index = bugs.findIndex(bug => bug.id === action.payload.id)
-        bugs[index].resolved = true
-    }
-})
-export function reducer(state = [], action) {
-    if(action.type === bugAdded.type)
-        return [
-            ...state,
-            {
+const slice = createSlice({
+    name: "bugs",
+    initialState: [],
+    reducers: {
+        bugAdded: (bugs, action) => {
+            bugs.push({
                 id: ++lastIndex,
                 description: action.payload.description,
                 resolved: false
-            }
-        ]
-    else if(action.type === bugRemoved.type)
-        return state.filter(bug => bug.id !== action.payload.id)
+            })
+        },
 
-    else if(action.type === bugResolved.type)
-        return state.map(bug => bug.id === action.payload.id ? {...bug, resolved:true} : bug)
+        bugResolved: (bugs, action) => {
+            const index = bugs.findIndex(bug => bug.id === action.payload.id)
+            bugs[index].resolved = true
+        }
+    }
+})
 
-    return state
-}
+export default slice.reducer
+export const { bugAdded, bugResolved } = slice.actions

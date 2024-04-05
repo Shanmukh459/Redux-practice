a462e82446c07dec7ca36477a635febf365a3351
Shanmukh459 <saikrrish459@gmail.com>
2024-04-04 19:58:30 -0700

    combining multiple stores

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import configureStore from "./store/configureStore"
-// import * as actions from './store/bugs'
-import * as actions from './store/projects'
+import * as actions from './store/bugs'
+import { projectAdded } from './store/projects'
+import { reducer } from "./store/reducer"
 
-const store = configureStore()
+
+
+const store = configureStore(reducer)
 store.subscribe(() => {
     console.log("State changed!")
 })
-// store.dispatch(actions.bugAdded({description: "Bug 1"}))
-// store.dispatch(actions.bugAdded({description: "Bug 2"}))
-// store.dispatch(actions.bugAdded({description: "Bug 3"}))
-// store.dispatch(actions.bugResolved({ id: 1}))
 
-store.dispatch(actions.projectAdded({name: "Project 1"}))
-store.dispatch(actions.projectAdded({name: "Project 2"}))
+console.log(store)
 
+store.dispatch(actions.bugAdded({description: "Bug 1"}))
+store.dispatch(actions.bugAdded({description: "Bug 2"}))
+store.dispatch(actions.bugAdded({description: "Bug 3"}))
+store.dispatch(actions.bugResolved({ id: 1}))
+
+store.dispatch(projectAdded({name: "Project 1"}))
+store.dispatch(projectAdded({name: "Project 2"}))
+
+const unresolvedBugs = store.getState().entities.bugs.filter(bug => !bug.resolved)
+console.log(unresolvedBugs)
 console.log(store.getState())

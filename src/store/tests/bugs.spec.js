7752c18145871f2f3de52649ac42b39f181076c6
Shanmukh459 <saikrrish459@gmail.com>
2024-04-11 13:41:07 -0700

    mocking http calls using axios-mock-adapter

--- src/store/tests/bugs.spec.js
+++ src/store/tests/bugs.spec.js
@@ -1,11 +1,27 @@
+import MockAdapter from "axios-mock-adapter"
+import axios from "axios"
 import { addBug } from "../bugs"
 import configureStore from "../configureStore"
 
+// describe("bugsSlice", () => {
+//     it("should handle addBug action", async () => {
+//         const store = configureStore()
+//         const bug = { description: 'a'}
+//         await store.dispatch(addBug(bug))
+//         expect(store.getState().entities.bugs.list).toHaveLength(1)
+//     })  
+// })
+
 describe("bugsSlice", () => {
-    it("should handle addBug action", async () => {
-        const store = configureStore()
+    it("Should handle addbug action", async () => {
         const bug = { description: 'a'}
+        const savedBug = { ...bug, id: 1}
+        const fakeAxios = new MockAdapter(axios)
+        fakeAxios.onPost('/bugs').reply(200, savedBug)
+
+        const store = configureStore()
         await store.dispatch(addBug(bug))
-        expect(store.getState().entities.bugs.list).toHaveLength(1)
-    })  
+        expect(store.getState().entities.bugs.list).toContainEqual(savedBug)
+
+    })
 })
